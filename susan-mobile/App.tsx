4ff2513cbf02c7a0f989f3d3c1964fc3f5a93604
Simon Frost <simonhfrost@gmail.com>
2025-06-17 23:17:11 -0600

    Don't log keys

--- susan-mobile/App.tsx
+++ susan-mobile/App.tsx
@@ -2,11 +2,9 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 import { SkiaAnimation } from 'components/SkiaAnimation';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-import { ELEVEN_LABS_KEY, BACKEND_URL } from '@env';
+// import { ELEVEN_LABS_KEY, BACKEND_URL } from '@env';
 
 import './global.css';
-
-console.log('ELEVEN_LABS_KEY:', ELEVEN_LABS_KEY);
 
 export default function App() {
   return (
