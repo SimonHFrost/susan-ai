79bcd88400107549388be0ea47b29d098a41dda4
Simon Frost <simonhfrost@gmail.com>
2025-06-17 23:04:43 -0600

    Use dotenv

--- susan-mobile/App.tsx
+++ susan-mobile/App.tsx
@@ -2,10 +2,11 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 import { SkiaAnimation } from 'components/SkiaAnimation';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { ELEVEN_LABS_KEY, BACKEND_URL } from '@env';
 
 import './global.css';
 
-console.log('ELEVEN_LABS_KEY:', process.env.ELEVEN_LABS_KEY);
+console.log('ELEVEN_LABS_KEY:', ELEVEN_LABS_KEY);
 
 export default function App() {
   return (
--- susan-mobile/app-env.d.ts
+++ susan-mobile/app-env.d.ts
@@ -1,2 +1,7 @@
 // @ts-ignore
 /// <reference types="nativewind/types" />
+
+declare module '@env' {
+  export const ELEVEN_LABS_KEY: string;
+  export const BACKEND_URL: string;
+}
